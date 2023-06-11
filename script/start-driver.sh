b3493752f878b71e4bac14b5a1a05169734b9272
Ivan Pohosov <ivan.pohosov@dev.pro>
2023-06-11 05:16:54 +0300

    Update port for

--- script/start-driver.sh
+++ script/start-driver.sh
@@ -6,7 +6,7 @@ if [ "$DISABLE_P2P_SYNC" == "false" ]; then
     taiko-client driver \
         --l1.ws ${L1_ENDPOINT_WS} \
         --l2.ws ws://l2_execution_engine:8546 \
-        --l2.auth http://l2_execution_engine:8551 \
+        --l2.auth http://l2_execution_engine:${PORT_L2_EXECTION_ENGINE_HTTP} \
         --taikoL1 ${TAIKO_L1_ADDRESS} \
         --taikoL2 ${TAIKO_L2_ADDRESS} \
         --jwtSecret /data/taiko-geth/geth/jwtsecret \
@@ -17,7 +17,7 @@ else
     taiko-client driver \
         --l1.ws ${L1_ENDPOINT_WS} \
         --l2.ws ws://l2_execution_engine:8546 \
-        --l2.auth http://l2_execution_engine:8551 \
+        --l2.auth http://l2_execution_engine:${PORT_L2_EXECTION_ENGINE_HTTP} \
         --taikoL1 ${TAIKO_L1_ADDRESS} \
         --taikoL2 ${TAIKO_L2_ADDRESS} \
         --jwtSecret /data/taiko-geth/geth/jwtsecret
